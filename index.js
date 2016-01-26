cca6d835f71194b66e43609d63dc22916c06dcea
Christian Amaral <darthcas@gmail.com>
2016-01-26 16:50:12 -0200

    adicionado suporte a tokens

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
 var assign = require('object-assign')
+var tokens = {}
+var isBrowser = typeof window !== 'undefined'
+var localStorageKeyName = 'authTokens'
+
+if (isBrowser) {
+  try {
+    assign(tokens, JSON.parse(window.localStorage.getItem(localStorageKeyName)))
+  } catch (e) {
+    // ~ no localStorage
+  }
+}
+function persist () {
+  try {
+    window.localStorage.setItem(localStorageKeyName, JSON.stringify(tokens))
+  } catch (e) {
+    // ~ dont bother
+  }
+}
+
+function setToken (origin, token) {
+  tokens[origin] = token
+  persist()
+}
+
+function getOrigin (url) {
+  return url.match(/^\/\//) || url.match(/:\/\//)
+    ? url.split('/').slice(0, 3).join('/')
+    : null
+}
 
 function toJSON (response) {
-  return response.status !== 204 && typeof response.json === 'function'
-    ? response.json()
-    : response
+  if (response.status === 204 || typeof response.json !== 'function') return response
+
+  return response.json()
+    .then(function (data) {
+      response.data = data
+      return response
+    })
 }
 
 function checkStatus (response) {
@@ -18,6 +51,10 @@ var sendsJson = {
   'Content-Type': 'application/json'
 }
 
+function extractToken (str) {
+  return typeof str === 'string' ? str.replace(/^Bearer\s/, '') : str
+}
+
 function apiFetch (endpoint, config) {
   var reqConfig = assign({}, config)
 
@@ -28,9 +65,26 @@ function apiFetch (endpoint, config) {
     assign(reqConfig.headers, sendsJson)
   }
 
+  if (isBrowser) {
+    var origin = getOrigin(endpoint) || window.location.origin
+
+    if (config.headers['Authorization']) {
+      setToken(origin, extractToken(config.headers['Authorization']))
+    } else if (tokens[origin]) {
+      config.headers['Authorization'] = 'Bearer ' + tokens[origin]
+    }
+  }
+
   return fetch(endpoint, reqConfig)
-    .then(checkStatus)
+    .then(function (response) {
+      if (isBrowser) {
+        setToken(origin,
+          extractToken(response.headers.get('Authorization')))
+      }
+      return response
+    })
     .then(toJSON)
+    .then(checkStatus)
 }
 
 function useMethod (method) {
@@ -39,6 +93,7 @@ function useMethod (method) {
   }
 }
 
+exports.setToken = setToken
 exports.GET = apiFetch
 exports.POST = useMethod('POST')
 exports.PUT = useMethod('PUT')
