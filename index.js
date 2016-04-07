b6eb8035579fcf0fcb17422e0cff89900b67317e
Christian Amaral <darthcas@gmail.com>
2016-04-07 14:59:11 -0300

    stack data

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function toJSON (response) {
     response.data = {
       message: 'Oopss... The API returned an invalid response'
     }
-    response.stack = new Error().stack
+    response.data.stack = new Error().stack
     return Promise.reject(response)
   }
 
